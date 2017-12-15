2cc98d47cd3774d35a2b164af0be9d47fa59e05e
jenadrianne <yan4414@gmail.com>
2017-12-15 21:31:09 +0800

    linear regression

--- src/es_miniproject/ES_MiniProject.java
+++ src/es_miniproject/ES_MiniProject.java
@@ -10,37 +10,61 @@ public class ES_MiniProject {
         String file_path = "C:\\Users\\User\\Documents\\NetBeansProjects\\ES_MiniProject\\src\\files\\Book1.csv"; 
         String line = "";
         String cvsSplitBy = ",";
-        double[][] table = new double[250][8];
+        double[][] table = new double[11][8];
         double[] summations = new double[8]; 
+        double slope, yintercept; 
         
+        // completing the table
         readFile(file_path, cvsSplitBy, table);
         solveXSqrd (table); 
         solveXY(table); 
         solveSummations(table, summations); 
         
-        displayContents(table,summations); 
+       
+        slope = solveforSlope(table.length, summations);
+        yintercept = solveforIntercept(table.length, summations, slope);
         
-
+        
+        solveYHat(table, slope, yintercept); 
+        solveErrors(table);
+        solveSummations(table, summations); 
+        
+        
+        displayContents(table,summations); 
+        displayLinerRegression (slope, yintercept);
     }
     
     public static void displayContents (double[][] table,  double[] summations){
         for(int x = 0 ; x < table.length ; x++){
             for(int y=0 ; y < 8; y++){
-                System.out.print(table[x][y] + " | "); 
+                System.out.printf(" %.4f | " , table[x][y] ); 
             }
             System.out.println("");
         }
         
         
-        System.out.println("**********************************************************************");
+        System.out.println("*****************************************************************************************************************");
          for(int y=0 ; y < 8; y++){
              switch(y){
-                 case 0 :  System.out.println("Summation of X: " + summations[y] ); break; 
-                 case 1 :  System.out.println("Summation of Y: " + summations[y] ); break; 
-                 case 2 :  System.out.println("Summation of X Sqrd: " + summations[y] ); break;
-                 case 3 :  System.out.println("Summation of XY: " + summations[y] ); break;
+                 case 0 :  System.out.printf("Summation of X: %.4f \n", summations[y] ); break; 
+                 case 1 :  System.out.printf("Summation of Y: %.4f \n",  summations[y] ); break; 
+                 case 2 :  System.out.printf("Summation of X Sqrd: %.4f \n", summations[y] ); break;
+                 case 3 :  System.out.printf("Summation of XY:  %.4f \n", summations[y] ); break;
+                 case 4 :  System.out.printf("Summation of Y':  %.4f \n", summations[y] ); break;
+                 case 5 : System.out.printf("Mean absulute error  %.4f \n", summations[y]/table.length ); break;
+                 case 6 : System.out.printf("Mean absolute percentage error:  %.4f \n", summations[y]/table.length ); break;
+                 case 7 : System.out.printf("Root mean squared error:  %.4f \n",  Math.sqrt(summations[y]/table.length)); break;
              }
         }
+    }
+    
+    public  static void displayLinerRegression (double slope, double intercept){
+         System.out.println("*****************************************************************************************************************");
+         System.out.println("LINEAR REGRESIION");
+         System.out.printf("Slope: %.4f \n",  slope); 
+         System.out.printf("Intercept: %.4f \n", intercept);
+         System.out.printf("Model: y = %.4fx + %.4f \n", slope, intercept); 
+         System.out.println("*****************************************************************************************************************");
     }
     public static void readFile( String file_path, String splitBy, double[][] table){
         String line = "";
@@ -70,11 +94,33 @@ public class ES_MiniProject {
        }
     }
     
+    public static void solveYHat(double[][] table, double slope, double intercept){
+        for(int x = 0 ; x < table.length ; x++ ){
+          table[x][4] = (table[x][0] *slope) +  intercept; 
+       }
+    }
+    
+    public static void solveErrors(double[][] table){
+        for(int x = 0 ; x < table.length ; x++ ){
+          table[x][5] = Math.abs(table[x][1] - table[x][4]);
+          table[x][6] = Math.abs((table[x][1] - table[x][4])/table[x][1]);
+          table[x][7] = Math.abs(table[x][5] * table[x][5]);
+       }
+    }
+    
     public static void solveSummations(double[][] table, double[] summations){
          for(int x = 0 ; x < table.length ; x++){
             for(int y=0 ; y < 8; y++){
                 summations[y] += table[x][y]; 
             }  
         }
+    }
+   
+    public static double solveforSlope(int size, double[] summations){
+        return ((size * summations[3]) - (summations[0] * summations[1]))/((size* summations[2])-(summations[0]*summations[0]));
+    }
+    
+     public static double solveforIntercept(int size, double[] summations, double slope){
+        return (summations[1] - (slope*summations[0]))/ size;
     }
 }
